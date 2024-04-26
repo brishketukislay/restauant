ce80a110bfd36b25bf5c72c0cff0ca2cb341579a
brishketukislay <brishketu.kislay@gmail.com>
2024-04-26 04:10:24 +0100

    test case

--- src/app/restaurent-dash/restaurent-dash.component.spec.ts
+++ src/app/restaurent-dash/restaurent-dash.component.spec.ts
@@ -22,4 +22,8 @@ describe('RestaurentDashComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should call', () => {
+    SVGPolygonElement()
+    expect(component).toBeTruthy();
+  });
 });
